1613b5d4d6e04a1d2bd9906ecd040763d766785c
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-07-12 20:44:27 +1000

    cool stuff

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 // index.js
 const express = require('express')
+const api = require ('./api/index')
 
 const app = express()
+app.use(express.json())
 const PORT = 4000
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
@@ -15,6 +12,11 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+
 
 // Export the Express API
 module.exports = app
