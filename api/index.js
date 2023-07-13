399698530a496db6cd3bc858f20742b383b28c50
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-07-13 11:49:19 +1000

    GAS GAS GAS

--- api/index.js
+++ api/index.js
@@ -1,10 +1,29 @@
 const app = require('express')();
 const { v4 } = require('uuid');
 const mongoose = require('mongoose'); // include mongodb package
+const bcrypt = require('bcrypt-nodejs')
 
 mongoose.set('strictQuery', true);
 const url = process.env.MONGODB_URI
 mongoose.connect(url)
+const db = mongoose.connection;
+db.on("error",(error)=>console.log(error));
+db.once("open",()=>console.log("DB Connected"));
+let accountSchema = new mongoose.Schema({
+  uid: Long,
+  username: String,
+  password: String
+});
+// hash the password
+accountSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+accountSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+var User = mongoose.model('user', accountSchema);
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
@@ -14,7 +33,17 @@ app.get('/api', (req, res) => {
 
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
+  
   res.end(`Item: ${slug}`);
 });
+app.post('/api/new/account', (req,res)=>{
+  var new_user = new User({
+    uid: Math.floor(Math.random() * (5000 - 20)) + 20,
+    username: req.body.username,
+    level: "new_member"
+  });
 
+  new_user.password = new_user.generateHash(req.body.password);
+  new_user.save();
+})
 module.exports = app;
